ab4fdffd0295a99ada056682d9f627b4e1f30caf
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-09-11 15:03:25 +0300

    add assets and google fonts

--- 04_website_projects/testimonial-grid/tailwind.config.js
+++ 04_website_projects/testimonial-grid/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md:'768px',
+    lg: '976px',
+    xl: '1440px',
+
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans:['Barlow Semi Condensed', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
